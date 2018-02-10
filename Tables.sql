9889e30553b8a89cef145703b01f5fa5ddd8f9c6
Hossam <hossam.elfar95@gmail.com>
2018-02-10 18:05:15 +0300

    Modyfing create tables script to handle the missing db and existing tables

--- Tables.sql
+++ Tables.sql
@@ -1,4 +1,11 @@
-CREATE TABLE USERS (
+-- create database
+CREATE DATABASE IF NOT EXISTS whatsapp;
+
+-- use created database
+USE whatsapp;
+
+-- Users table
+CREATE TABLE IF NOT EXISTS USERS (
  id SERIAL NOT NULL,
  mobile_number VARCHAR(20),
  display_name VARCHAR(100) NOT NULL,
@@ -10,7 +17,8 @@ CREATE TABLE USERS (
  PRIMARY KEY (mobile_number)
 );
 
-CREATE TABLE CHATS (
+-- chats table
+CREATE TABLE IF NOT EXISTS CHATS (
  id SERIAL,
  mobile_number VARCHAR(20) NOT NULL,
  is_archived BOOLEAN NOT NULL DEFAULT FALSE,
@@ -20,7 +28,8 @@ CREATE TABLE CHATS (
  FOREIGN KEY (mobile_number) REFERENCES USERS (mobile_number)
 );
 
-CREATE TABLE GROUP_CHATS (
+-- group chats table
+CREATE TABLE IF NOT EXISTS GROUP_CHATS (
  id SERIAL,
  group_name VARCHAR(100) NOT NULL,
  display_picture TEXT,
@@ -30,7 +39,8 @@ CREATE TABLE GROUP_CHATS (
  FOREIGN KEY (created_by) REFERENCES USERS (mobile_number)
 );
 
-CREATE TABLE GROUP_CHAT_MEMBERS (
+-- group chats members table
+CREATE TABLE IF NOT EXISTS GROUP_CHAT_MEMBERS (
  id SERIAL,
  group_chat_id INT NOT NULL,
  mobile_number VARCHAR(20) NOT NULL,
@@ -43,7 +53,8 @@ CREATE TABLE GROUP_CHAT_MEMBERS (
  FOREIGN KEY (mobile_number) REFERENCES USERS (mobile_number)
 );
 
-CREATE TABLE BLOCKED (
+-- blocked users table
+CREATE TABLE IF NOT EXISTS BLOCKED (
  id SERIAL,
  blocker_mobile_number VARCHAR(20) NOT NULL,
  blocked_mobile_number VARCHAR(20) NOT NULL,
@@ -52,7 +63,8 @@ CREATE TABLE BLOCKED (
  FOREIGN KEY (blocked_mobile_number) REFERENCES USERS (mobile_number)
 );
 
- CREATE TABLE REPORTED (
+-- reported users table
+ CREATE TABLE IF NOT EXISTS REPORTED (
  id SERIAL,
  reporter_mobile_number VARCHAR(20) NOT NULL,
  reported_mobile_number VARCHAR(20) NOT NULL,
