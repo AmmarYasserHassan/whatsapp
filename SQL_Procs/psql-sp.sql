4480c998e6d2c5ee1880cb9fd26200568195dee8
Hossam <hossam.elfar95@gmail.com>
2018-02-10 18:40:35 +0300

    Adding stored procedure to add a new user

--- SQL_Procs/psql-sp.sql
+++ SQL_Procs/psql-sp.sql
@@ -1,5 +1,5 @@
 -- Procedure to report a user
-CREATE FUNCTION report_user(reporter_mobile_number VARCHAR(70), reported_mobile_number VARCHAR(70))
+CREATE OR REPLACE FUNCTION report_user(reporter_mobile_number VARCHAR(70), reported_mobile_number VARCHAR(70))
 RETURNS void AS $$
 BEGIN
   INSERT INTO reported VALUES (reporter_mobile_number, reported_mobile_number);
@@ -7,9 +7,21 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Procedure to block a user
-CREATE FUNCTION block_user(blocker_mobile_number VARCHAR(70), blocked_mobile_number VARCHAR(70))
+CREATE OR REPLACE FUNCTION block_user(blocker_mobile_number VARCHAR(70), blocked_mobile_number VARCHAR(70))
 RETURNS void AS $$
 BEGIN
   INSERT INTO blocked VALUES (blocker_mobile_number, blocked_mobile_number);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Procedure to add a new user
+CREATE OR REPLACE FUNCTION insert_user(mobile_number VARCHAR(20), display_name VARCHAR(100), 
+    display_picture TEXT, user_status TEXT, verification_code UUID)
+RETURNS void AS $$
+BEGIN
+  INSERT INTO users
+    (mobile_number ,display_name , display_picture, user_status, verification_code )
+   VALUES
+    (mobile_number , display_name , display_picture , user_status, verification_code);
 END;
 $$ LANGUAGE plpgsql;
