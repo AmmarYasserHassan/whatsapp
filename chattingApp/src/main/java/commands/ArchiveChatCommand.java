5ce196366f010eb2f6a3550ef7d0270634ae7808
MernaMaklad <merna.maklad@gmail.com>
2018-03-03 19:14:04 +0200

    Added 'archivechatcommand' waiting for the SP archive_chat to be implemented

---
+++ chattingApp/src/main/java/commands/ArchiveChatCommand.java
@@ -0,0 +1,49 @@
+package commands;
+
+
+import database.DBHandler;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ArchiveChatCommand {
+
+    DBHandler dbHandler;
+    String userNumber;
+    int chatId;
+    boolean isGroupChat;
+
+    /**
+     * Constructor
+     *
+     * @param dbHandler
+     * @param userNumber
+     * @param chatId
+     * @param isGroupChat
+     *
+     */
+    public ArchiveChatCommand(DBHandler dbHandler, String userNumber, int chatId, boolean isGroupChat)
+    {
+        this.dbHandler = dbHandler;
+        this.userNumber = userNumber;
+        this.chatId = chatId;
+        this.isGroupChat = isGroupChat;
+    }
+
+    /**
+     * Execute the archive chat ommand
+     * check if it's a group chat or a normal chat then enter the table and archive it
+     * @return Result Set
+     * @throws SQLException
+     */
+    public ResultSet execute() throws SQLException {
+        String archive_chat;
+        if (isGroupChat)
+            archive_chat= "SELECT archive_group_chat(" + "'" + userNumber + "'" + ", " + "'" + chatId + "'"+ ");";
+        else
+            archive_chat= "SELECT archive_chat(" + "'" + userNumber + "'" + ", " + "'" + chatId + "'"+ ");";
+
+        return this.dbHandler.executeSQLQuery(archive_chat);
+    }
+}
