6d1cfcaf3b7a28fc2c32afee222cab8404ae3498
AhmedAmirK <ahmedkhalaf24@gmail.com>
2018-03-17 19:31:32 +0200

    followed chattingApp structure

---
+++ storiesApp/src/main/java/commands/Command.java
@@ -0,0 +1,8 @@
+package commands;
+
+import org.json.JSONObject;
+
+public interface Command {
+
+    public JSONObject execute();
+}
---
+++ storiesApp/src/main/java/sender/MqttSender.java
@@ -0,0 +1,41 @@
+package sender;
+
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
+
+public class MqttSender {
+    private final static String QUEUE_NAME = "response";
+    private ConnectionFactory factory;
+    private Channel channel;
+    private Connection connection;
+
+    public MqttSender() throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, TimeoutException {
+        factory = new ConnectionFactory();
+        factory.setUri("localhost");
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    }
+
+    public void send(JSONObject res) throws IOException {
+        channel.basicPublish("", QUEUE_NAME, null, res.toString().getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + res.toString() + "'");
+    }
+
+    public void close() throws IOException, TimeoutException {
+        channel.close();
+        connection.close();
+    }
+
+
+}
