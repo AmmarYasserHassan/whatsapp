e2bb2561c64e5d3e6cb1a7b9710b187193c61da3
sedki-abdelhakim <sedki.abdelhakim@gmail.com>
2018-03-23 02:08:40 +0200

    Add test logic

--- authApp/src/main/java/commands/ValidateTokenCommand.java
+++ authApp/src/main/java/commands/ValidateTokenCommand.java
@@ -1,4 +1,5 @@
 package commands;
+
 import java.io.UnsupportedEncodingException;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
@@ -9,57 +10,54 @@ import com.google.gson.JsonObject;
 import org.json.JSONException;
 import org.json.JSONObject;
 import sender.MqttSender;
-public class ValidateTokenCommand implements Command, Runnable{
+
+public class ValidateTokenCommand implements Command, Runnable {
 
     String userNumber;
-    String token ;
+    String token;
 
     /**
      * Constructor
      * @param request
      */
 
-    public ValidateTokenCommand(JsonObject request)  {
+    public ValidateTokenCommand(JsonObject request) {
         super();
         this.userNumber = request.get("userNumber").getAsString();
-        this.token  = request.get("jwt").getAsString();
+        this.token = request.get("jwt").getAsString();
 
     }
 
-
     public JSONObject execute() {
-        JSONObject res= new JSONObject();
-        String valid = "true";
+        JSONObject res = new JSONObject();
+        boolean valid = true;
+        boolean error = false;
         try {
 
             // Start validate JWT
             Algorithm algorithm = Algorithm.HMAC256("secret");
-            JWTVerifier verifier = JWT.require(algorithm)
-                    .withIssuer("USER:"+userNumber)
-                    .withClaim("scope", "USER")
-                    .withClaim("userNumber", userNumber)
-                    .build(); //Reusable verifier instance
+            JWTVerifier verifier = JWT.require(algorithm).withIssuer("USER:" + userNumber).withClaim("scope", "USER")
+                    .withClaim("userNumber", userNumber).build(); //Reusable verifier instance
             DecodedJWT jwt = verifier.verify(token);
             // End validate JWT
 
-        }
-        catch (JSONException e) {
+        } catch (JSONException e) {
+            error = true;
             e.printStackTrace();
 
         } catch (UnsupportedEncodingException exception) {
+            error = true;
             //UTF-8 encoding not supported
             exception.printStackTrace();
 
-        } catch (JWTVerificationException exception){
-            //Invalid signature/claims
-            valid = "false";
+        } catch (JWTVerificationException exception) {
+            valid = false;
         }
-      
-        res.put("valid",valid);
 
+        res.put("valid", valid);
+        res.put("error", error);
         return res;
     }
-
 
     public void run() {
         JSONObject res = this.execute();
@@ -67,10 +65,29 @@ public class ValidateTokenCommand implements Command, Runnable{
             MqttSender sender = new MqttSender();
             sender.send(res);
             sender.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
 
         }
     }
+
+    //begin testing block
+    /*
+    public static void validateToken() {
+        JsonObject request = new JsonObject();
+        //for the fail test case change the user number or the token 
+        request.addProperty("userNumber", "01000000001");
+        request.addProperty("jwt",
+                "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZSI6IlVTRVIiLCJpc3MiOiJVU0VSOjAxMDAwMDAwMDAxIiwidXNlck51bWJlciI6IjAxMDAwMDAwMDAxIn0.IWpLuHkcUfPCW2dckGm2fYkxatrYgwRm0ybjFC_fxbo");
+        ValidateTokenCommand validate = new ValidateTokenCommand(request);
+        JSONObject res = validate.execute();
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) {
+        validateToken();
+    }
+    */
+    //end testing block
 
 }
