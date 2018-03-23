89d339c9fe8d9c75f32560bbb62ec20821ddf54e
sedki-abdelhakim <sedki.abdelhakim@gmail.com>
2018-03-23 02:15:00 +0200

    fixed Unreachable catch block

--- authApp/src/main/java/database/MongoDBConnection.java
+++ authApp/src/main/java/database/MongoDBConnection.java
@@ -7,45 +7,41 @@ import com.mongodb.MongoClientURI;
 import java.net.UnknownHostException;
 
 public class MongoDBConnection {
-	
+
 	private MongoClient mongoClient;
 	private DB database;
 	private String basicURI;
 	private String databaseName;
 	private String username;
 	private String password;
-	
-	
+
 	/**
-     * DBConnection constructor.
-     * This constructor uses environment variables to for the database options.
-     */
-    public MongoDBConnection() {
-    	mongoClient = null;
-    	database = null;
-    	basicURI = "localhost:27017";
-    	databaseName = "mydb";
-    	username = "Username";
-    	password = "Password";
+	 * DBConnection constructor.
+	 * This constructor uses environment variables to for the database options.
+	 */
+	public MongoDBConnection() {
+		mongoClient = null;
+		database = null;
+		basicURI = "localhost:27017";
+		databaseName = "mydb";
+		username = "Username";
+		password = "Password";
 	}
 
-    /**
-     * Connect to the database.
-     *
-     * @return database object.
-     * @see {@link DB}
-     */
-    DB connect(){
-//    	+username+":"+password+"@"
-    	String uri = "mongodb://"+basicURI;
-    	MongoClientURI mongoClientURI = new MongoClientURI(uri);
-    	try {
-			mongoClient = new MongoClient(mongoClientURI);
-			database = mongoClient.getDB(databaseName);
-		} catch (UnknownHostException e) {
-			System.err.println("Cannot connect to mongoDB !");
-		} 
-    	return database;
-    }
-	
+	/**
+	 * Connect to the database.
+	 *
+	 * @return database object.
+	 * @see {@link DB}
+	 */
+	DB connect() {
+		//    	+username+":"+password+"@"
+		String uri = "mongodb://" + basicURI;
+		MongoClientURI mongoClientURI = new MongoClientURI(uri);
+		mongoClient = new MongoClient(mongoClientURI);
+		database = mongoClient.getDB(databaseName);
+
+		return database;
+	}
+
 }
